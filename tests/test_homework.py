e5c2a49df7ac0ced5c11fbbf11750d40a0e7fa4f
Daniel Orozco R <daorozcore@unal.edu.co>
2025-01-26 20:48:21 -0500

    Commit 2

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,10 +1,43 @@
 """Autograding script."""
 
-import os
+
+def load_data():
+
+    import pandas as pd
+
+    dataset = pd.read_csv("files/input/heart_disease.csv")
+    y = dataset.pop("target")
+    x = dataset.copy()
+    x["thal"] = x["thal"].map(
+        lambda x: "normal" if x not in ["fixed", "fixed", "reversible"] else x
+    )
+
+    return x, y
+
+
+def load_estimator():
+
+    import os
+    import pickle
+
+    if not os.path.exists("homework/estimator.pickle"):
+        return None
+    with open("homework/estimator.pickle", "rb") as file:
+        estimator = pickle.load(file)
+
+    return estimator
 
 
 def test_01():
-    """Test word count job."""
 
-    assert os.path.exists("files/output/summary.csv")
-    assert os.path.exists("files/plots/top10_drivers.png")
+    from sklearn.metrics import accuracy_score
+
+    x, y = load_data()
+    estimator = load_estimator()
+
+    accuracy = accuracy_score(
+        y_true=y,
+        y_pred=estimator.predict(x),
+    )
+
+    assert accuracy > 0.8613
